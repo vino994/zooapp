c3b3835f76184a3151ea7efd2de4c031a81a7147
vinothkumar sanjeevi <vinodjayasudha@gmail.com>
2025-09-01 10:40:09 +0100

    prepare for deployment

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
+// src/App.js
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import ZooSlider from "./ZooSlider";
 
 function App() {
   return (
-    <div>
-      <ZooSlider />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<ZooSlider />} />
+      </Routes>
+    </Router>
   );
 }
 
